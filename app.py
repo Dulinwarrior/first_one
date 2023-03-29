ddfd8f34b6b087b9bb5a2281420c468b5ecc78c5
Dulinwarrior <128140548+Dulinwarrior@users.noreply.github.com>
2023-03-28 19:49:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-options = ['hello', 'world']
+options = ['The history of cars🕰️', 'Difference between DIESEL and PETROL engines⛽', 'Modern capabilitties of cars🚗', 'Concept Cars🏎️']
 st.sidebar.selectbox('Options', options)
-st.title('hello world')
+st.title('All you need to know about cars')
 st.write('This is my first web hosted application')
